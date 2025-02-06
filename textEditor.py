d07fd8551593432400fcd874618d71b03f0a291d
Bao Nguyen <baowao3015872@gmail.com>
2025-02-05 19:12:10 -0500

    update

--- textEditor.py
+++ textEditor.py
@@ -1,5 +1,5 @@
 import Tkinter import *
-import tFileDialog import *
+import tkFileDialog import *
 
 filename = None
 
@@ -7,3 +7,37 @@ def newFile():
     global filename
     filename = "Untitled"
     text.delete(0.0, END)
+
+def saveFile():
+    global filename
+    t = text.get(0.0, END)
+    f = open(filename, 'w')
+    f.write(t)
+    f.close()
+
+def saveAs():
+    f = asksaveasfile(mode='w', defaultextension='.txt')
+    t = text.get(0.0, END)
+    try:
+        f.write(t.rstrip())
+    except:
+        showerror(title="Oops!", message="Unable to save file...")
+
+def openFile():
+    f = askopenfile(mode='r')
+    t = f.read()
+    text.delete(0.0, END)
+    text.insert(0.0, t)
+
+root = Tk()
+root.title("My Python Text Editor")
+root.minsize(width=400, height=400)
+root.maxsize(width=400, height=400)
+
+text = Text(root, width=400,height=400)
+text.pack()
+
+menubar = Menu(root)
+filemenu = Menu(menubar)
+filemenu.add_command(label="New", command=newFile)
+filemenu.add_command(label="Open", command=openFile)
